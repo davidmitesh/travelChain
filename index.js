f0a6cf6dc7fc61063383ace56abf1a6e916963ef
davidmitesh <excelrock_mitesh@yahoo.com>
2020-02-04 21:48:07 +0545

    /addUser route added

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express=require('express');
 let port = process.env.PORT;
-const app=express();
 var mongoose= require('./server/db/mongoose.js');
 var {User}= require('./server/models/user.js');
+const _=require('lodash');
+var bodyParser= require('body-parser');
+let app=express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.get('/',(req,res)=>{
     res.send('Hey welcome to the platform');
 });
 
 
-app.get('/addUser',(req,res)=>{
+app.post('/addUser',(req,res)=>{
+    User.countDocuments({},(err,count)=>{
+        var newComer = new User({ name: req.body.name,uid:count+1,tokens:10,gender:req.body.gender,verifier:req.body.verifier });
+        newComer.save().then(() => res.send("successfully added"));
+    })
 
-
-const kitty = new User({ name: 'Zildjian' });
-kitty.save().then(() => res.send("successfully added"));
 });
 if (port == null || port == "") {
   port = 3000;
