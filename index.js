158ff99e818a87f88fee9cf070bdeff70fb2eb3c
davidmitesh <excelrock_mitesh@yahoo.com>
2020-02-04 20:33:38 +0545

    attempt1

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const express=require('express');
 let port = process.env.PORT;
 const app=express();
 var mongoose= require('./server/db/mongoose.js');
+var {User}= require('./server/models/user.js');
 app.get('/',(req,res)=>{
     res.send('Hey welcome to the platform');
 });
-var userSchema = new mongoose.Schema({
-    name:String
-});
-const Cat = mongoose.model('Cat', userSchema);
+
+
 app.get('/addUser',(req,res)=>{
 
 
-const kitty = new Cat({ name: 'Zildjian' });
+const kitty = new User({ name: 'Zildjian' });
 kitty.save().then(() => res.send("successfully added"));
 });
 if (port == null || port == "") {
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-
-// mongoose.connect('mongodb+srv://excelrock_mitesh:krishnatara@travelchain-sz05q.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true });
-mongoose.connect('mongodb://localhost:27017/travelchain',{ useNewUrlParser: true });
+const db = process.env.MONGODB_URL;
+mongoose.connect(db,{ useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/travelchain',{ useNewUrlParser: true });
 module.exports={mongoose};
