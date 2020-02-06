6d65a7dade997bf20e670ac65582b89c80d1cb35
davidmitesh <excelrock_mitesh@yahoo.com>
2020-02-06 17:31:49 +0545

    frontend removed

--- ipfsonly.js
+++ ipfsonly.js
@@ -2,13 +2,37 @@ const ipfsAPI = require('ipfs-api');
 const ipfs = new ipfsAPI({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 const fs= require('fs');
 
-let testFile = fs.readFileSync("new.json");
-// //Creating buffer for ipfs function to add file to the system
-let testBuffer = new Buffer(testFile);
+// let testFile = fs.readFileSync("new.json");
+// // //Creating buffer for ipfs function to add file to the system
+// let testBuffer = new Buffer(testFile);
 
-ipfs.files.add(testBuffer, function (err, file) {
-    if (err) {
-      console.log(err);
-    }
+// ipfs.files.add(c, function (err, file) {
+//     if (err) {
+//       console.log(err);
+//     }
+//
+// console.log(file);})
+const input = [
+  {
+    'id': '0x10',
+    'date': '14.07.2018'
+  },
+  {
+    'id': '0x20',
+    'date': '14.07.2018'
+  },
+  {
+    'id': '0x30',
+    'date': '14.07.2018'
+  }
+]
 
-console.log(file);})
+ipfs.files.add(Buffer.from(JSON.stringify(input)))
+  .then(res => {
+    const hash = res[0].hash
+    console.log('added data hash:', hash)
+    return ipfs.files.cat(hash)
+  })
+  .then(output => {
+    console.log('retrieved data:', JSON.parse(output))
+  })
