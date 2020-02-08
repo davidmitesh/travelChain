727bde436a36faa4d1a7997bc2f702811f3aea4e
davidmitesh <excelrock_mitesh@yahoo.com>
2020-02-08 23:50:47 +0545

    serverdeploycorrected

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,10 @@
 var mongoose=require('mongoose');
 
 mongoose.Promise=global.Promise;
-// var db = process.env.MONGODB_URL;
-// if (db == null || db == "") {
-//   db = "mongodb+srv://excelrock_mitesh:krishnatara@travelchain-sz05q.mongodb.net/test?retryWrites=true&w=majority";
-// }
-// mongoose.connect(db,{ useNewUrlParser: true });
-mongoose.connect('mongodb://localhost:27017/travelchain',{ useNewUrlParser: true });
+var db = process.env.MONGODB_URL;
+if (db == null || db == "") {
+  db = "mongodb+srv://excelrock_mitesh:krishnatara@travelchain-sz05q.mongodb.net/test?retryWrites=true&w=majority";
+}
+mongoose.connect(db,{ useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/travelchain',{ useNewUrlParser: true });
 module.exports={mongoose};
